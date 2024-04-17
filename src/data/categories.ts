d6b304f5cbc0667fde774cd3361256bdfada1cd3
SergeiKrk <krjukov_s@mail.ru>
2024-04-17 16:12:12 +0400

    добавил статью

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,2 +1,9 @@
 // List of categories for blog posts
-export const CATEGORIES = ['Пол', 'Интерьер', 'Полезные советы', 'Спальня', 'Другое'] as const
+export const CATEGORIES = [
+	'Пол',
+	'Интерьер',
+	'Полезные советы',
+	'Спальня',
+	'Отделка',
+	'Другое'
+] as const
