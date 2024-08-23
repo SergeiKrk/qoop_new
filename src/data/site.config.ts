a7d7e0bd5ce878b91dda8d5b7a0baea0d9488c7d
SergeiKrk <krjukov_s@mail.ru>
2024-08-23 17:38:58 +0400

    добавил title и description

--- src/data/site.config.ts
+++ src/data/site.config.ts
@@ -10,8 +10,9 @@ interface SiteConfig {
 
 export const siteConfig: SiteConfig = {
 	author: 'SergeiKrk', // Site author
-	title: 'SamogonCalc - онлайн-калькуляторов самогонщика', // Site title.
-	description: 'Набор быстрых онлайн-калькуляторов для винокуров и самогонщиков', // Description to display in the meta tags
+	title: 'CUBlog - блог о дизайне интерьеров, декоре, ремонте и отделке', // Site title.
+	description:
+		'Добро пожаловать на блог о дизайне интерьеров, декоре, ремонте и отделке. Здесь вы найдете вдохновение, советы по выбору материалов и мебели, а также практические руководства по ремонту. Узнайте о современных трендах в интерьере и создайте стильное пространство вместе с нами.', // Description to display in the meta tags
 	lang: '	ru-RU',
 	ogLocale: 'ru_RU',
 	shareMessage: 'Поделиться постом', // Message to share a post on social media
